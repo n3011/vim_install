f42041f4b11f7b0579db5df5f70a968ece5200e4
n3011 <mrinal.haloi11@gmail.com>
2017-07-19 20:11:20 +0530

    minor update

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,6 @@
-sudo apt-get install vim
-
+sudo apt-get install -y vim
+sudo pip install pep8
+sudo pip install autopep8
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
